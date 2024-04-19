09d32b2ad4e795f5ad717fff0c25817b34657b8a
Noah Peden <noahpeden@gmail.com>
2024-04-18 22:36:22 -0400

    Feat: publish package

---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
+
+export default {
+  input: 'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' },
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+  external: [],
+};
